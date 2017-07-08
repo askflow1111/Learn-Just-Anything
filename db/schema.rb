38fddeaccf0795fceac19e33dee0869ae2a74a01
askflow1111 <askflow1111@gmail.com>
2017-07-08 17:27:40 -0400

    rails

--- db/schema.rb
+++ db/schema.rb
@@ -46,9 +46,9 @@ ActiveRecord::Schema.define(version: 20170611093939) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.text "picture"
     t.text "about"
     t.index ["email"], name: "index_users_on_email", unique: true
